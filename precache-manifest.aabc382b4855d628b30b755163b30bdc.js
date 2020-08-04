db074e308696ae34b1e9712efee83173a1e33248
Alberto <alberto512c@gmail.com>
2020-08-04 19:44:55 +0200

    Updates

---
+++ precache-manifest.aabc382b4855d628b30b755163b30bdc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60ecc8e6737527331435c93845f9639",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "616d8bfdfaed4ad44737",
+    "url": "/Recipes/static/css/2.61c015ba.chunk.css"
+  },
+  {
+    "revision": "e941e1070fcd39e922bc",
+    "url": "/Recipes/static/css/main.4e167560.chunk.css"
+  },
+  {
+    "revision": "616d8bfdfaed4ad44737",
+    "url": "/Recipes/static/js/2.cbf9e5af.chunk.js"
+  },
+  {
+    "revision": "be0d2f9fab4a70348f1c57b7de2ec23a",
+    "url": "/Recipes/static/js/2.cbf9e5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e941e1070fcd39e922bc",
+    "url": "/Recipes/static/js/main.63508f60.chunk.js"
+  },
+  {
+    "revision": "e12db099ca5af2a8ce76",
+    "url": "/Recipes/static/js/runtime-main.ea483e6a.js"
+  },
+  {
+    "revision": "b8e0d132eb66261167b9e48055519e09",
+    "url": "/Recipes/static/media/Background.b8e0d132.jpg"
+  },
+  {
+    "revision": "ad5aa8cc82b8b2f772b518af7e23fd45",
+    "url": "/Recipes/static/media/NavBarBackground.ad5aa8cc.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Recipes/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Recipes/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Recipes/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Recipes/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Recipes/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Recipes/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Recipes/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Recipes/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Recipes/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Recipes/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Recipes/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Recipes/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Recipes/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Recipes/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Recipes/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Recipes/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Recipes/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
